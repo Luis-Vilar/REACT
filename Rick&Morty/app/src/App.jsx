5e66a4826ef381574c673d5ad03312b5fff2796f
Luis Federico Vilar Velazquez <luis_velazquez@estudante.sesisenai.org.br>
2023-03-20 17:58:56 -0400

    refactorizacion card personaje

--- Rick&Morty/app/src/App.jsx
+++ Rick&Morty/app/src/App.jsx
@@ -1,33 +1,38 @@
+// importamos useState y useEffect de React
 import { useState, useEffect } from "react";
+// importamos el componente Personajes
 import Personajes from "./components/Personajes";
-
+// function  del compónente App()
 function App() {
+  // funcion asincrona para obtener los datos de la API
   async function fetchData(state) {
+    // fetch() es una función nativa de JavaScript que permite hacer peticiones HTTP
+    // await espera a que la petición se resuelva
+
     const response = await fetch("https://rickandmortyapi.com/api/character");
     const data = await response.json();
+    // colocamos los datos en el estado que recibimos por parámetro
     state(data);
   }
 
+  // useEffect() es una función nativa de React que permite ejecutar
+  // código cuando el componente se monta o se actualiza
+  // en este caso, cuando el componente se monta, ejecutamos la función fetchData()
   useEffect(() => {
     fetchData(setPersonajes);
   }, []);
-
+  // declaramos el estado personajes y su función para actualizarlo
   const [personajes, setPersonajes] = useState(null);
   console.log(personajes);
 
   return (
-    <div className="App container ">
-      <h1 className="text-bg-primary " >Rick & Morty</h1>
+    <div className="container">
+      <h1 >Rick & Morty</h1>
 
       {personajes ? (
         <div>
           {personajes.results.map((personaje) => (
-            <Personajes
-              status={personaje.status}
-              id={personaje.id}
-              image={personaje.image}
-              name={personaje.name}
-            />
+            <Personajes personaje={personaje} />
           ))}
         </div>
       ) : (
--- Rick&Morty/app/src/components/Personajes.jsx
+++ Rick&Morty/app/src/components/Personajes.jsx
@@ -1,16 +1,40 @@
+// componente que recibe un personaje con el cual crea una tarjeta
+
 function Personajes(props) {
+  // copiamos en una constante el objeto personaje que recibimos por props
+  const personaje = props.personaje;
+
+  // retornamos el JSX que queremos que se muestre
   return (
-    <div className="card border badge bg-secondary text-bg-primary" style={{width: "30em"}} key={props.id}>
+    <div
+      className="card border badge bg-secondary text-bg-primary "
+      style={{ width: "25rem", margin: "0 auto" }}
+      key={personaje.id}
+    >
       {/* Nombre */}
-      <h3>{`${props.name} `}</h3>
-      {/* status */}
-      <h5>{`Status : ${props.status}`}</h5>
-      <span className="border "></span>
+      <h3>{`${personaje.name} `}</h3>
       {/* Imagen */}
-      <img className="img-fluid"src={props.image} alt={props.name} style={{borderRadius : "50%" }}  />
+      <img
+        className="img-fluid"
+        src={personaje.image}
+        alt={personaje.name}
+        style={{ borderRadius: "50%" }}
+      />
+      {/* status */}
+      <h5>{`Status : ${personaje.status}`}</h5>
+      {/* Especie */}
+      <h5>{`Especie : ${personaje.species}`}</h5>
+      {/* Genero */}
+      <h5>{`Genero : ${personaje.gender} `}</h5>
+      {/* domicilio */}
+      <h5>{`Domicilio actual : ${personaje.location.name} `}</h5>
+      {/* domicilio actual */}
+      <h5>{`Domicilio natal : ${personaje.origin.name} `}</h5>
       {/*  */}
     </div>
   );
 }
 
+
+// exportamos el componente para poder usarlo en otros archivos
 export default Personajes;
